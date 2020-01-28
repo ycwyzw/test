db4ddc9732966fe0caaa6e28b91423159cc67b57
ycwyzw <ycwyzw@163.com>
2020-01-28 09:41:59 +0800

    second

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println(1);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
